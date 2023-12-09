cb6cc5d418dcb249adf7d4b02ce4edd73a986e7f
Lukas <razilgry@gmail.com>
2023-12-09 06:52:15 +0100

    Dodaj pliki projektów.

---
+++ PetlaFor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        
+        for (double x = 0; x <= 10; x++)
+        {
+        
+            double y  = Math.Sqrt(Math.Pow(3, 2)) * x;
+
+            Console.WriteLine($"y = {y}");
+
+        }
+    }
+}
